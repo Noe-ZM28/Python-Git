8c3a4bee784f3f3df2f6957f33acbb8635329976
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-11 00:51:40 -0500

    haciendo el modelo para el login de usuarios

---
+++ Proyecto_Dj/AgregarUsuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Usuarios(models.Model):
+    usuario = models.CharField(max_length= 35)
+    correo = models.EmailField()
+    contraseña = models.CharField(max_length=30)
+    roll = models.CharField(max_length=25)
