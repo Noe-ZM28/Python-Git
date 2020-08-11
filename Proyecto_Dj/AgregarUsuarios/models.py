baec2fd958bd434f3652ce05ba002415b5b2c652
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-11 11:02:33 -0500

    Creando la base de datos

--- Proyecto_Dj/AgregarUsuarios/models.py
+++ Proyecto_Dj/AgregarUsuarios/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-
 class Usuarios(models.Model):
-    usuario = models.CharField(max_length= 35)
+    usuario = models.CharField(max_length= 30)
     correo = models.EmailField()
     contraseña = models.CharField(max_length=30)
     roll = models.CharField(max_length=25)
