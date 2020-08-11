211d63e581ff7aaf8a1f47dc2e06e04ed0552baa
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-11 15:17:15 -0500

    modifique algo de la tabla de usuarios

--- Proyecto_Dj/AgregarUsuarios/admin.py
+++ Proyecto_Dj/AgregarUsuarios/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-
 # Register your models here.
+from AgregarUsuarios.models import Usuarios #importar el modelo para agregar usuarios
 
-from AgregarUsuarios.models import Usuarios
 
-admin.site.register(Usuarios)
+class MostrarCampos(admin.ModelAdmin):#heredar de la clase admin y modificar
+    list_display=("usuario",  "correo", "roll") #Mostrar campos
+    search_fields=("usuario", "correo", "roll") # agregar barra de busqueda 
+
+admin.site.register(Usuarios, MostrarCampos) #agregar el modelo a la pantalla de administrador de Django y el metodo mostrar campos
+
--- Proyecto_Dj/AgregarUsuarios/models.py
+++ Proyecto_Dj/AgregarUsuarios/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Usuarios(models.Model):
+class Usuarios(models.Model): # modelo para agregar usuarios
+
     usuario = models.CharField(max_length= 30)
     correo = models.EmailField()
     contraseña = models.CharField(max_length=30)
     roll = models.CharField(max_length=25)
+
+#    def __str__(self):
+#        return self.usuario
