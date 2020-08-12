1a1698d71b6498f9ce69e66bfc0735b3513f2cac
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-11 21:32:14 -0500

    trabajando en la tarjeta

--- Proyecto_Dj/AgregarUsuarios/models.py
+++ Proyecto_Dj/AgregarUsuarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,5 +10,3 @@ class Usuarios(models.Model): # modelo para agregar usuarios
     contraseña = models.CharField(max_length=30)
     roll = models.CharField(max_length=25)
 
-#    def __str__(self):
-#        return self.usuario
