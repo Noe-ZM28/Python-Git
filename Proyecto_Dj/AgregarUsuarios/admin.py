726cd608c3d0570f20496ae4b301b7a52f3e76a3
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-12 17:44:45 -0500

    Solucione el error que tenia en la funcion para ingredar nuevos usuarios

--- Proyecto_Dj/AgregarUsuarios/admin.py
+++ Proyecto_Dj/AgregarUsuarios/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from AgregarUsuarios.models import Usuario #importar el modelo para agregar usuarios
 
 
-#class MostrarCampos(admin.ModelAdmin):#heredar de la clase admin y modificar
-#    list_display=("usuario",  "correo", "roll") #Mostrar campos
-#    search_fields=("usuario", "correo", "roll") # agregar barra de busqueda 
+class MostrarCampos(admin.ModelAdmin):#heredar de la clase admin y modificar
+    list_display=("username",  "email", "roll") #Mostrar campos
+    search_fields=("username",  "email", "roll") # agregar barra de busqueda 
 
-admin.site.register(Usuario)#, MostrarCampos) #agregar el modelo a la pantalla de administrador de Django y el metodo mostrar campos
+admin.site.register(Usuario, MostrarCampos)#, MostrarCampos) #agregar el modelo a la pantalla de administrador de Django y el metodo mostrar campos
 
--- Proyecto_Dj/AgregarUsuarios/models.py
+++ Proyecto_Dj/AgregarUsuarios/models.py
@@ -1,38 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager , AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 class UsuarioManager (BaseUserManager):
-    def create_user(self, email, username, roll, password):
+    def create_user(self, username, email, roll, password): 
         if not email:
             raise ValueError('Los usuarios deben de tener un correo electronico valido')
-        
+
         usuario = self.model(
                             username = username,
                             email = self.normalize_email(email),
                             roll = roll, )
-
         usuario.set_password(password)
         usuario.save()
         return usuario
 
-    def create_superuser(self, username, email, roll, password, **kwargs):
-        usuario = self.create_superuser(
+    def create_superuser(self, username, email, roll, password):
+        usuario = self.create_user(
                             username= username,
                             email= email,
                             roll= roll,
-                            password= password, **kwargs)
-        
+                            password= password)
+
         usuario.usuario_administrador = True
         usuario.save()        
-        return usuario()
+        return usuario
 
 class Usuario(AbstractBaseUser): # modelo para agregar usuarios
     username = models.CharField('Nombre de usuario', unique=True, max_length= 30)
     email = models.EmailField('Correo electronico', unique=True)
     roll = models.CharField('rol', max_length=25)
-    #password = models.CharField(max_length=30)
 
-    
+    #password = models.CharField(max_length=30)
     usuario_activo = models.BooleanField(default = True)
     usuario_administrador = models.BooleanField(default = False)
     objects = UsuarioManager()
