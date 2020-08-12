58ab41205db182ea0a98e57d8c420d6dc877e734
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-12 11:52:19 -0500

    Sin cambios tratando de arreglar el error

--- Proyecto_Dj/AgregarUsuarios/models.py
+++ Proyecto_Dj/AgregarUsuarios/models.py
@@ -7,20 +7,21 @@ class UsuarioManager (BaseUserManager):
             raise ValueError('Los usuarios deben de tener un correo electronico valido')
         
         usuario = self.model(
-                            email = self.normalize_email(email),
                             username = username,
-                            roll = roll)
+                            email = self.normalize_email(email),
+                            roll = roll, )
 
         usuario.set_password(password)
         usuario.save()
         return usuario
 
-    def create_superuser(self, email, username, roll, password):
+    def create_superuser(self, username, email, roll, password):
         usuario = self.create_superuser(
-                            email = email,
                             username= username,
-                            roll = roll, 
-                            password = password)
+                            email= email,
+                            roll= roll,
+                            password= password)
+        
         usuario.usuario_administrador = True
         usuario.save()        
         return usuario()
@@ -28,24 +29,29 @@ class UsuarioManager (BaseUserManager):
 
 
 class Usuario(AbstractBaseUser): # modelo para agregar usuarios
-    email = models.EmailField('Correo electronico', unique=True)
     username = models.CharField('Nombre de usuario', unique=True, max_length= 30)
-    password = models.CharField(max_length=30)
+    email = models.EmailField('Correo electronico', unique=True)
     roll = models.CharField('rol', max_length=25)
+    #password = models.CharField(max_length=30)
+
     
-    usuario_activo = models.BooleanField(default=False)
-    usuario_administrador = models.BooleanField(default=False)
+    usuario_activo = models.BooleanField(default = True)
+    usuario_administrador = models.BooleanField(default = False)
     objects = UsuarioManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'password', 'roll']
+    REQUIRED_FIELDS = ['email', 'roll', 'password']
 
-    
+    def __str__(self):
+        return f'{self.username}, {self.email}, {self.roll}'
+
     def has_perm(self, perm, obj = None):
         return True
+    
     def has_module_perms(self, app_label):
         return True
 
     @property
     def is_staff(self):
         return self.usuario_administrador
+    
