274789e4388be4c8c0ce8c8db859bbe1b0258a77
Noe Zuñiga Morales <noe.zuniga@metrics.digital@gmail.com>
2020-08-18 15:41:12 -0500

    cree una rama para trabajar en el error del login

--- Proyecto_Dj/AgregarUsuarios/views.py
+++ Proyecto_Dj/AgregarUsuarios/views.py
@@ -20,14 +20,14 @@ def dashboard(request):
     user = request.user
     auth0user = user.social_auth.get(provider='auth0')
     userdata = {
-        #'user_id': auth0user.uid,
+        'user_id': auth0user.uid,
         #'name': user.first_name,
         'picture': auth0user.extra_data['picture'],
         'email': auth0user.extra_data['email']  }
 
     return render(request, 'dashboard.html', {
         'auth0User': auth0user,
-        'userdata': json.dumps(userdata, indent=2)
+        'userdata': json.dumps(userdata, indent=3)
     })
 
 def logout(request):
